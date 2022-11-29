4e7132a6ebced14153851db8efd3b90cf35a561f
Vucelj Ahmed <vucljahmed5227@gmail.com>
2022-11-29 20:16:54 +0100

    JavaScript

---
+++ JavaScript - Cas 1/app.js
@@ -0,0 +1,79 @@
+//Primitivni
+
+// String
+// Number
+// Boolean mogu biti true ili false
+// undefined
+// null
+// Symbol
+
+// //Referemti
+
+// Array 
+// Object
+// function
+
+tekst = "Hello World";
+
+broj = 25;
+
+// Boolean - true or false
+
+polozenHTML = true;
+
+imePrezime = "Ajla Gudzevic"
+console.log(polozenHTML)
+console.log(imePrezime)
+
+console.log(broj)
+console.log(tekst)
+console.log(typeof tekst);
+console.log(typeof broj);
+
+//undefind
+
+let age;
+console.log(age);
+
+let nekiTekst = null;
+console.log(nekiTekst)
+
+ime = 'Ajla';
+prezime = 'Gudzevic';
+
+imePrezime = ime + ' ' + prezime;
+
+//Logicki operatori
+// && i
+// || ili 
+// ! negacija
+
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
+
+console.log(!true)
+console.log(!false)
+
+//Poredjenje ==
+
+prviBroj = 10;
+drugiBroj = 15;
+
+console.log(prviBroj == drugiBroj)
+
+// ===
+
+console.log(prviBroj === drugiBroj)
+
+// !=
+// !==
+
+console.log(prviBroj != drugiBroj)
+console.log(prviBroj !== drugiBroj)
---
+++ JavaScript - Cas 2/app.js
@@ -0,0 +1,71 @@
+ime = "Lejla";
+ime = "Amina";
+console.log(ime);
+
+prviBroj = 10;
+console.log(prviBroj);
+
+console.log("--------------------------");
+prviBroj = prviBroj + 1;
+console.log(prviBroj);
+console.log("--------------------------");
+prviBroj++;
+console.log(prviBroj);
+
+prviBroj--;
+
+prviBroj = prviBroj + 10;
+prviBroj += 10;
+prviBroj -= 10;
+prviBroj *= 10;
+prviBroj /= 10;
+
+console.log(20 % 7);
+
+// if (uslov) {
+//   //ako je uslov ispunjen
+// } else {
+//   //ako uslov nije ispunjen
+// }
+
+
+ime = "Ajla";
+
+if (ime === "Ajla") {
+  console.log("Zdravo ", ime);
+}
+
+//Zadatak 1
+//Inicijalizovati broj i proveriti da li je pozitivan ili negativan
+
+m = 0;
+
+if (m > 0) {
+  console.log("broj je pozitivan");
+} else {
+  console.log("Broj je negativan");
+}
+
+//Uneti dva broja i proveriti da li su ta dva broja jednaka
+
+a = 10;
+b = 25;
+if (a === b) {
+  console.log("Brojevi su isti");
+} else {
+  console.log("Brojevi nisu isti");
+}
+
+console.log(true !== "false");
+
+a = 25;
+a++;
+a = 10;
+a--;
+a *= 2;
+
+console.log(a % 4);
+
+// utorak 19:30
+// sreda 18:00
+// subota 13:30
