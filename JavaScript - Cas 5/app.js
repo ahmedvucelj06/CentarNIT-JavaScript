c4d21ccc43b27ab1f76643de3083871abc2fde27
Vucelj Ahmed <vucljahmed5227@gmail.com>
2022-12-06 19:33:36 +0100

    JavaScript

---
+++ JavaScript - Cas 5/app.js
@@ -0,0 +1,27 @@
+// console.log("---Zadatak #1---");
+// a = Number(prompt("Unesite neki broj\n"));
+
+// for(i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+// for(i = 10; i >= 0; i--){
+//     console.log(i);
+// }
+
+// for(i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+zvezdice = "";
+a = Number(prompt("Unesite broj redova\n"));
+for (i = 1; i <=a; i++){
+    zvezdice += i;
+    console.log(zvezdice);
+}
+
+// broj = "";
+// a = Number(prompt("Unesite broj redova\n"));
+// for (i = 1; i <=a; i++){
+//     console.log(broj);
+// }
+
+
