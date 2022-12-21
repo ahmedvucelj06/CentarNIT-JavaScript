371ef994ea584182eb4f67af7ce22930fc0b91b5
Vucelj Ahmed <vucljahmed5227@gmail.com>
2022-12-21 19:18:23 +0100

    JavaScript

--- JavaScript - Cas 12/app.js
+++ JavaScript - Cas 12/app.js
@@ -1,40 +1,40 @@
-// brojac = 0;
-// str = 'Mama ima Momu';
-// for ( i = 0; i < str.length; i++){
-//     if ( str[i] === 'm' || str[i] === 'M'){
-//     brojac++;
-//     }
-// }
-// console.log(brojac);
+brojac = 0;
+str = 'Mama ima Momu';
+for ( i = 0; i < str.length; i++){
+    if ( str[i] === 'm' || str[i] === 'M'){
+    brojac++;
+    }
+}
+console.log(brojac);
 
 
-// rec = prompt("Unesite rec koju zelite: ");
-// recbrojac = 0;
-// for ( i = 0; i < rec.length; i++){
-//     if ( rec[i] == rec[i].toLowerCase()){
-//         recbrojac++;
-//     }
-// }
-// console.log(recbrojac);
+rec = prompt("Unesite rec koju zelite: ");
+recbrojac = 0;
+for ( i = 0; i < rec.length; i++){
+    if ( rec[i] == rec[i].toLowerCase()){
+        recbrojac++;
+    }
+}
+console.log(recbrojac);
 
-// rec2 = prompt("Unesite rec koju zelite: ");
-// recbrojac2 = 0;
-// recbrojac3 = 0;
-// for ( i = 0; i < rec2.length; i++){
-//     if ( rec2[i] == rec2[i].toLowerCase() && rec2 != ' '){
-//         recbrojac2++;
-//     }
-//     if ( rec2[i] == rec2[i].toUpperCase() && rec2 != ' '){
-//         recbrojac3++;
-//     }
-// }
-// if ( recbrojac2 > recbrojac3){
-//     console.log('Ima vise malih slova');
-// } if ( recbrojac2 < recbrojac3) {
-//     console.log('Ima vise velikih slova');
-// }else {
-//     console.log('Jednaki su');
-// }
+rec2 = prompt("Unesite rec koju zelite: ");
+recbrojac2 = 0;
+recbrojac3 = 0;
+for ( i = 0; i < rec2.length; i++){
+    if ( rec2[i] == rec2[i].toLowerCase() && rec2 != ' '){
+        recbrojac2++;
+    }
+    if ( rec2[i] == rec2[i].toUpperCase() && rec2 != ' '){
+        recbrojac3++;
+    }
+}
+if ( recbrojac2 > recbrojac3){
+    console.log('Ima vise malih slova');
+} if ( recbrojac2 < recbrojac3) {
+    console.log('Ima vise velikih slova');
+}else {
+    console.log('Jednaki su');
+}
 
 // JavaScript Variables 
 
@@ -44,9 +44,9 @@
 //Using nothing
 
 
-num = 5;
-const arraysDays = ["Ponedeljak", "Utorak", "Sreda", "Cetvrtak", "Petak", "Subota", "Nedelja"]
-console.log(arraysDays[num - 1]);
+// num = 5;
+// const arraysDays = ["Ponedeljak", "Utorak", "Sreda", "Cetvrtak", "Petak", "Subota", "Nedelja"]
+// console.log(arraysDays[num - 1]);
 
 // JavaScript Switch
 
@@ -86,3 +86,26 @@ switch(num){
     default:
         console.log('Greska');
 }
+
+
+nizBrojeva = [1, 2, 3, 4, 1, 2, 3, 5, 6, 6, 7]
+noviBrojevi = []
+
+for ( let i = 0; i<nizBrojeva.length; i++){
+    isInside = false;
+    for( let j = i+1; j<noviBrojevi.length; j++){
+        if ( nizBrojeva[i] === nizBrojeva[j] ){
+            isInside = true;
+            break;
+        }
+    }
+
+    if ( isInside ){
+        continue;
+    }else {
+        noviBrojevi.push(nizBrojeva[i]);
+    }
+}
+
+console.log(noviBrojevi);
+
